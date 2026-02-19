46a69455da3d3cf799f43c2956deb818e4f6bc3a
Pan-cham <e.adarsh06@gmail.com>
2026-02-19 11:25:37 +0530

    Second commit

---
+++ x.py
@@ -0,0 +1,8 @@
+#KNN
+from sklearn.neighbors import KNeighborsClassifier
+X = [[30],[40],[50],[60],[20],[10],[70]]
+y = [0,1,1,1,0,0,1]
+classifier= KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2 )
+classifier.fit(X,y)
+X_marks=[[50]]
+print(classifier.predict(X_marks))
